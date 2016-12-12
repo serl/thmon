d83c95a06344bdf830a4661232615e649a959ee9
Sergio Livi <livi@i3s.unice.fr>
2016-12-12 12:06:46 +0100

    double read sensor, pep8 refactoring

--- libs/read_value.py
+++ libs/read_value.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python
 
 import sys
+from time import sleep
 import Adafruit_DHT
 
-humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
 
-if humidity is not None and temperature is not None:
-    print 'TEMPERATURE={0:0.1f} HUMIDITY={1:0.1f}'.format(temperature, humidity)
-else:
-    print 'TEMPERATURE= HUMIDITY='
-    sys.exit(1)
+def main():
+    """read value from sensor"""
+
+    number_of_reads = 2
+    pause_between_reads = 3  # seconds
+    for i in range(number_of_reads):
+        val = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+        if val == (None, None):
+            print 'TEMPERATURE= HUMIDITY='
+            sys.exit(1)
+        f_val = 'TEMPERATURE={0:0.1f} HUMIDITY={1:0.1f}'.format(val[1], val[0])
+        print >> sys.stderr, "measure {}: {}".format(i + 1, f_val)
+        sleep(pause_between_reads)
+    print f_val
+
+
+if __name__ == '__main__':
+    main()
--- thingspeak_push.py
+++ thingspeak_push.py
@@ -8,33 +8,42 @@ import config
 from libs.http import HTTP
 os.chdir(os.path.dirname(sys.argv[0]))  # so that workdir = this dir
 
-http = HTTP({'THINGSPEAKAPIKEY': config.API_KEY})
 
-if not hasattr(config, 'limit_uploads'):
-    config.LIMIT_UPLOADS = 5
+def main():
+    """Uploads all files in cache to Thingspeak"""
 
-if not hasattr(config, 'delay'):
-    config.DELAY = 16
+    http = HTTP({'THINGSPEAKAPIKEY': config.API_KEY})
 
-cached = sorted([int(f) for f in os.listdir('./cache') if f.isdigit()])[0:config.LIMIT_UPLOADS]
-for timestamp in cached:
-    contents = ''
-    filename = './cache/{}'.format(timestamp)
-    with open(filename, 'r') as cnts:
-        contents = cnts.read().strip()
-    (temperature, humidity) = contents.split(' ')
-    isodate = datetime.utcfromtimestamp(timestamp).isoformat()
-    error = None
-    try:
-        result = http.call(
-            'update', 'POST', {'field1': temperature, 'field2': humidity, 'created_at': isodate})
-        if result == '0':
-            error = 'Thingspeak exception'
-        print result
-    except:
-        error = 'Network error'
-    if error is not None:
-        print "Error uploading data: {}".format(error)
-        sys.exit(1)
-    os.remove(filename)
-    sleep(config.DELAY)
+    if not hasattr(config, 'limit_uploads'):
+        config.LIMIT_UPLOADS = 5
+
+    if not hasattr(config, 'delay'):
+        config.DELAY = 16
+
+    whole_cache = [int(f) for f in os.listdir('./cache') if f.isdigit()]
+    cached = sorted(whole_cache)[0:config.LIMIT_UPLOADS]
+    for timestamp in cached:
+        contents = ''
+        filename = './cache/{}'.format(timestamp)
+        with open(filename, 'r') as cnts:
+            contents = cnts.read().strip()
+        (temperature, humidity) = contents.split(' ')
+        isodate = datetime.utcfromtimestamp(timestamp).isoformat()
+        error = None
+        data_to_send = {
+            'field1': temperature, 'field2': humidity, 'created_at': isodate}
+        try:
+            result = http.call('update', 'POST', data_to_send)
+            if result == '0':
+                error = 'Thingspeak exception'
+            print result
+        except:
+            error = 'Network error'
+        if error is not None:
+            print "Error uploading data: {}".format(error)
+            sys.exit(1)
+        os.remove(filename)
+        sleep(config.DELAY)
+
+if __name__ == "__main__":
+    main()
