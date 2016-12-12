79e31d75240d461637b70c74ce035834c9ae764c
Sergio Livi <livi@i3s.unice.fr>
2016-12-12 11:35:13 +0100

    pep8(?)

--- libs/http.py
+++ libs/http.py
@@ -1,20 +1,28 @@
-import json, httplib, urllib
+"""
+Wrapper for HTTP requests to thingspeak service.
+"""
+
+import json
+import httplib
+import urllib
+
 
 class HTTP:
-	def __init__(self, default_headers):
-		self.default_headers = default_headers.copy()
 
-	def call(self, endpoint, verb='GET', params={}, input_headers={}):
-		headers = self.default_headers.copy()
-		headers.update(input_headers)
-		connection = httplib.HTTPSConnection('api.thingspeak.com', 443)
-		body = ''
-		connection.connect()
-		url = '/{}'.format(endpoint)
-		if verb == 'GET':
-			url += '?{}'.format(urllib.urlencode(params))
-		else:
-			body = json.dumps(params)
-			headers.update({"Content-Type": "application/json"})
-		connection.request(verb, url, body, headers)
-		return connection.getresponse().read()
+    def __init__(self, default_headers):
+        self.default_headers = default_headers.copy()
+
+    def call(self, endpoint, verb='GET', params={}, input_headers={}):
+        headers = self.default_headers.copy()
+        headers.update(input_headers)
+        connection = httplib.HTTPSConnection('api.thingspeak.com', 443)
+        body = ''
+        connection.connect()
+        url = '/{}'.format(endpoint)
+        if verb == 'GET':
+            url += '?{}'.format(urllib.urlencode(params))
+        else:
+            body = json.dumps(params)
+            headers.update({"Content-Type": "application/json"})
+        connection.request(verb, url, body, headers)
+        return connection.getresponse().read()
