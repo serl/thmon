7dbd7cb33331edc82ca51a7323f72f5bafc56bc5
Sergio Livi <me@serl.it>
2019-01-20 19:36:44 +0100

    updated to new ThingSpeak API (bulk update)

--- thingspeak_push.py
+++ thingspeak_push.py
@@ -5,23 +5,23 @@ import os
 from datetime import datetime
 from time import sleep
 import config
-from libs.http import HTTP
+from libs.thingspeak import ThingSpeak
 os.chdir(os.path.dirname(sys.argv[0]))  # so that workdir = this dir
 
 
 def main():
     """Uploads all files in cache to Thingspeak"""
 
-    http = HTTP({'THINGSPEAKAPIKEY': config.API_KEY})
+    ts_client = ThingSpeak(config.TS_CHANNEL_ID, config.TS_WRITE_API_KEY)
 
-    if not hasattr(config, 'LIMIT_UPLOADS'):
-        config.LIMIT_UPLOADS = 5
-
-    if not hasattr(config, 'DELAY'):
-        config.DELAY = 16
+    if not hasattr(config, 'TS_BULK_UPDATE_SIZE'):
+        config.TS_BULK_UPDATE_SIZE = 50
 
     whole_cache = [int(f) for f in os.listdir('./cache') if f.isdigit()]
-    cached = sorted(whole_cache)[0:config.LIMIT_UPLOADS]
+    cached = sorted(whole_cache)[0:config.TS_BULK_UPDATE_SIZE]
+    entries = []
+    files = []
+    error = None
     for timestamp in cached:
         contents = ''
         filename = './cache/{}'.format(timestamp)
@@ -29,25 +29,29 @@ def main():
             contents = cnts.read().strip()
         (temperature, humidity) = contents.split(' ')
         isodate = datetime.utcfromtimestamp(timestamp).isoformat()
-        error = None
-        data_to_send = {
+        entry = {
             'field1': temperature, 'field2': humidity, 'created_at': isodate}
         if float(humidity) >= 50:
             # using approximated formula that works only in these conditions
             dew_point = float(temperature) - ((100 - float(humidity)) / 5)
-            data_to_send['field3'] = "{:.1f}".format(dew_point)
-        try:
-            result = http.call('update', 'POST', data_to_send)
-            if result == '0':
-                error = 'Thingspeak exception'
-            print result
-        except:
-            error = 'Network error'
-        if error is not None:
-            print "Error uploading data: {}".format(error)
-            sys.exit(1)
-        os.remove(filename)
-        sleep(config.DELAY)
+            entry['field3'] = "{:.1f}".format(dew_point)
+        entries.append(entry)
+        files.append(filename)
+
+    try:
+        result = ts_client.bulk_update(entries)
+        if 'error' in result:
+            error = '{message} ({details})'.format(**result['error'])
+    except Exception as e:
+        error = 'Network error ({})'.format(e.message)
+    if error:
+        print "Error uploading data: {}".format(error)
+        sys.exit(1)
+
+    for f in files:
+        os.remove(f)
+
+    print "{} entries updated".format(len(entries))
 
 if __name__ == "__main__":
     main()
